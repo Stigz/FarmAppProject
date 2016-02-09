9816bca254db19db57a31a9b6198d5e22aae8045
patclittle <plittle@Patricks-MacBook-Pro-6.local>
2016-02-08 20:22:24 -0800

    Initiating Xcode project

---
+++ FarmApp/FarmApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FarmApp
+//
+//  Created by Patrick Little on 8/02/2016.
+//  Copyright © 2016 Patrick Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
