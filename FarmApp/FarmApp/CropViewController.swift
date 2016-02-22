579396e971414d0bf50cc0a55c20633680b9082c
patclittle <plittle@Patricks-MacBook-Pro-6.local>
2016-02-21 17:32:03 -0800

    Added harvest functionality
    Still need to update bed + crop history when this happens

--- FarmApp/FarmApp/CropViewController.swift
+++ FarmApp/FarmApp/CropViewController.swift
@@ -18,6 +18,14 @@ class CropViewController: UIViewController {
     @IBOutlet weak var harvestedButton: UIButton!
     @IBOutlet weak var notesField: UITextView!
     
+    //Initially hidden labels
+    @IBOutlet weak var enterDateLabel: UILabel!
+    @IBOutlet weak var dayInput: UITextField!
+    @IBOutlet weak var monthInput: UITextField!
+    @IBOutlet weak var yearInput: UITextField!
+    @IBOutlet weak var harvestButton: UIButton!
+    
+    
     var crop : Crop!
     var bedNum : Int = 0
     var sectNum : Int = 0
@@ -60,6 +68,45 @@ class CropViewController: UIViewController {
     //Close the current screen -- back button clicked
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //When user wants to harvest the crop
+    @IBAction func harvest() {
+        harvestedButton.hidden = true
+        enterDateLabel.hidden = false
+        dayInput.hidden = false
+        monthInput.hidden = false
+        yearInput.hidden = false
+        harvestButton.hidden = false
+    }
+    
+    //TO enter the harvest date
+    @IBAction func addHarvestDate() {
+        if let day : Int? = Int(dayInput.text!){
+            if let month : Int? = Int(monthInput.text!){
+                if let year : Int? = Int(yearInput.text!){
+                    let newHarvest = Date(year: year!, month: month!, day: day!)
+                    crop.dateHarvested = newHarvest
+                    //TO-DO: Set up a way for this to alert the bed that it no longer has this
+                    //crop planted
+                    showHarvestDate()
+                }
+            }
+        }
+        
+    }
+    
+    //To show new harvest date
+    func showHarvestDate(){
+        harvestedButton.hidden = false
+        harvestedButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        harvestedButton.setTitle("Harvested: \(crop.dateHarvested.printSlash())", forState: .Normal)
+        harvestedButton.userInteractionEnabled = false
+        enterDateLabel.hidden = true
+        dayInput.hidden = true
+        monthInput.hidden = true
+        yearInput.hidden = true
+        harvestButton.hidden = true
     }
     
     //So that tapping on the view dismisses the keyboard
